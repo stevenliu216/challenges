1bed8ceaa9350377d86089be155cdab560bf4df3
Dan Yeaw <dan@yeaw.me>
2019-04-17 10:12:07 -0400

    Cleanup assert statements
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

--- tests/test_challenge27_pytest.py
+++ tests/test_challenge27_pytest.py
@@ -2,13 +2,13 @@ from challenges.challenge27 import reach_the_finish
 
 
 def test_finish_line():
-    assert reach_the_finish([2, 3, 1, 1, 4]) is True
+    assert reach_the_finish([2, 3, 1, 1, 4])
     assert reach_the_finish([2, 0, 0])
 
 
 def test_run_out_of_range():
-    assert reach_the_finish([3, 2, 1, 0, 4]) is False
+    assert not reach_the_finish([3, 2, 1, 0, 4])
 
 
 def test_no_race():
-    assert reach_the_finish([0]) is True
+    assert reach_the_finish([0])
